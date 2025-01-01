2062080e60718abc132a5fe4025dd821c2128ac0
wdoyle123 <will.doyle30@gmail.com>
2025-01-01 17:39:50 +0000

    feat: response model

--- app/orm_main.py
+++ app/orm_main.py
@@ -1,6 +1,6 @@
 import os
 from random import randrange
-from typing import Optional
+from typing import Optional, List
 
 import psycopg
 from fastapi import Depends, FastAPI, HTTPException, Response
@@ -32,13 +32,13 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.post("/posts", status_code=201)
+@app.post("/posts", status_code=201, response_model=schemas.Post)
 async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -47,7 +47,7 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 
-@app.get("/posts/{id}", status_code=201)
+@app.get("/posts/{id}", status_code=201, response_model=schemas.Post)
 async def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
@@ -65,7 +65,7 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=204)
 
 
-@app.put("/posts/{id}", status_code=201)
+@app.put("/posts/{id}", status_code=201, response_model=schemas.Post)
 async def update_post(
     id: int, update_post: schemas.PostCreate, db: Session = Depends(get_db)
 ):
@@ -75,4 +75,4 @@ async def update_post(
         raise HTTPException(status_code=404, detail=f"post with id: {id} was not found")
     post_query.update(update_post.dict())
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
